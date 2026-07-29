3bcac6a0d622910b7f646a6eb7b1f52073b6f436
mj4617384-art <mj4617384@gmail.com>
2026-07-29 05:16:52 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
-
+import Home from './pages/Home'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />   <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
